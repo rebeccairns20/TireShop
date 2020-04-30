f7393a455b4ea7ed526dfc67b91e9a0002d82b70
rebeccairns20 <59659375+rebeccairns20@users.noreply.github.com>
2020-04-30 16:14:28 -0400

    Create login.php

---
+++ login.php
@@ -0,0 +1,11 @@
+<div id="welcomeSign">
+   <p id="Moto">Are You </br> Tired Of</br> Your Tires?</p>
+</div>
+<div id="signIn">
+   <form action="../view/loginVerify.php" id="loginForm" method="post" >
+     <p id="signIN">Sign In</p>
+      <label>Username: </label><input type="text" name="username"></br>
+      <label>Password: </label> <input type="password"  name="password"></br>
+      <input type="submit" value="Login">
+   </form>
+</div>
