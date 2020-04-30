f64b8528fa21657c0efccd3ffa37e666e6010489
rebeccairns20 <59659375+rebeccairns20@users.noreply.github.com>
2020-04-30 16:14:19 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html lang="eng">
+   <head>
+      <meta charset="utf-8">
+      <title>Rubber Meets The Road Tire Shop</title>
+      <link rel="stylesheet" type="text/css"
+          href="../main.css">
+   </head>
+   <body>
+      <div id="main">
+         <h1>Ruber Meets The Road</h1>
+         <h4>Tire Company</h4>
+         <hr>
